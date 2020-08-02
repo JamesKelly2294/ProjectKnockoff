52d9b450f4bbe3f58eaa9755c7a9da97666134a5
Morgan Cabral <morgan.cabral@gmail.com>
2020-08-01 18:07:56 -0700

    First pass on the state machine for turn management

---
+++ Assets/Project/Turn Management/BaseTurnState.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public abstract class BaseTurnState : MonoBehaviour
+{
+    /// <summary>
+    /// The Turn 
+    /// </summary>
+    private TurnStateMachineManager stateMachineManager;
+
+    /// <summary>
+    /// Handle any incoming events which may result in the state transitioning or require some kind of response.
+    /// </summary>
+    /// <param name="manager">The state machine manager instance that this state belongs to</param>
+    /// <param name="eventType">The event which occured</param>
+    public abstract void HandleTurnStateEvent(TurnStateMachineManager manager, TurnStateMachineManagerEventType eventType);
+
+    /// <summary>
+    /// States in the State Machine should implement this instead of Update so they can have access to the turn manager.
+    /// </summary>
+    /// <param name="manager">The state machine manager instance that this state belongs to</param>
+    public abstract void OnUpdate(TurnStateMachineManager manager);
+
+    /// <summary>
+    /// Called on the current state when a transition to the next state is about to occur.
+    /// </summary>
+    /// <param name="manager">The state machine manager instance that this state belongs to</param>
+    /// <param name="nextState">The next state that the state machine will transition to</param>
+    public virtual void OnTransitionFromTurnState(TurnStateMachineManager manager, BaseTurnState nextState)
+    {
+        Debug.Log($"Transitioning away from {this.name}.");
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Gives the next state that will be transitioned to an opportunity to perform any required setup.
+    /// </summary>
+    /// <param name="manager">The state machine manager instance that this state belongs to</param>
+    public virtual void OnTransitionIntoTurnState(TurnStateMachineManager manager)
+    {
+        Debug.Log($"Transitioning into {this.name}.");
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Pass in a state machine which will manage this state's lifecycle.
+    /// 
+    /// You *probably* don't want to use this unless you know what you're doing
+    /// or have some need for passing a state between state machines.
+    /// </summary>
+    /// <param name="manager">The state machine manager instance that this state will belong to</param>
+    public void ConfigureTurnManager(TurnStateMachineManager manager)
+    {
+        this.stateMachineManager = manager;
+    }
+
+    // TODO: LifeCycle method for triggering a final transition into a terminal state in TurnStateMachine?
+
+    public void Awake()
+    {
+        // States are explicitly disabled upon starting, and only become active
+        // when a TurnManager or something else explicitly enables them.
+        // The TurnManager does its own housekeeping of the current (active) state.
+        gameObject.SetActive(false);
+    }
+
+    public void Start()
+    {
+        // Is this really useful?
+        Debug.Log($"Starting {gameObject.name}.");
+    }
+
+    public void Update()
+    {
+        // Injects the state machine manager into the concrete state implementation.
+        // This could be an explicit field on the base type, but I don't think
+        // we want to introduce the possibility of miswiring state machines
+        // if we get to a point where we're nesting them inside of states.
+        OnUpdate(this.stateMachineManager);
+    }
+}
---
+++ Assets/Project/Turn Management/TurnStateMachineManager.cs
@@ -0,0 +1,121 @@
+﻿using UnityEditor.UI;
+using UnityEngine;
+
+/// <summary>
+/// Possible events which can result in the state machine for the turn transitioning to the next state.
+/// </summary>
+public enum TurnStateMachineManagerEventType
+{
+    /// <summary>
+    /// The player started their turn.
+    /// </summary>
+    PlayerBeginTurn,
+
+    /// <summary>
+    /// The player ended their turn.
+    /// </summary>
+    PlayerEndTurn,
+
+    /// <summary>
+    /// The player activated a Mana.
+    /// </summary>
+    PlayerManaActivated,
+
+    /// <summary>
+    /// The player deactivated a Mana.
+    /// </summary>
+    PlayerManaDeactivated,
+
+    /// <summary>
+    /// The player began a drag event starting from a spell.
+    /// </summary>
+    PlayerBeginSpellDrag,
+
+    /// <summary>
+    /// The player began a drag event starting from a rune.
+    /// </summary>
+    PlayerBeginRuneDrag,
+
+    /// <summary>
+    /// The drag event ended on a specific monster controlled by the BigBad.
+    /// </summary>
+    PlayerEndDragOnEnemyMonster,
+
+    /// <summary>
+    /// The drag event ended in a way where an AOE attack should hit all (or a subset?) of the enemy's monsters.
+    /// </summary>
+    PlayerEndDragOnEnemyAOE, 
+
+    /// <summary>
+    /// The drag event ended on the overall enemy.
+    /// </summary>
+    PlayerEndDragOnBigBad,
+
+    /// <summary>
+    /// The drag event ended on a rune.
+    /// </summary>
+    PlayerEndDragOnRune,
+
+    /// <summary>
+    /// The drag event ended on the discard pile.
+    /// </summary>
+    PlayerEndDragOnDiscard,
+
+    /// <summary>
+    /// The drag event ended on the board or some other unactionable entity.
+    /// </summary>
+    PlayerEndDragUnspecified,
+}
+
+public class TurnStateMachineManager : MonoBehaviour
+{
+    /// <summary>
+    /// The initial state to start the state machine with.
+    /// </summary>
+    public BaseTurnState startingState;
+
+    /// <summary>
+    ///  The current state. 
+    /// </summary>
+    private BaseTurnState currentState;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Debug.Log("Starting the TurnManager state machine");
+        currentState = startingState;
+        currentState.ConfigureTurnManager(this);
+        currentState.OnTransitionIntoTurnState(this);
+        currentState.Start();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // TODO: Should TurnManager handle Player <-> Enemy turn logic on its own, or should that be another state machine?
+
+        // TODO: Handle terminal state logic here?
+    }
+
+    /// <summary>
+    /// Forcibly transition the state machine to the given state.
+    /// </summary>
+    /// <param name="nextState">The state that will be transition into</param>
+    public void TransitionToState(BaseTurnState nextState)
+    {
+        currentState.OnTransitionFromTurnState(this, nextState);
+        currentState = nextState;
+        currentState.ConfigureTurnManager(this);
+        currentState.OnTransitionIntoTurnState(this);
+    }
+
+    /// <summary>
+    /// Handle events which may result in the state machine transitioning into another state.
+    /// This delegates to the current state to handle the event.
+    /// </summary>
+    /// <param name="eventType">The type of event which occured</param>
+    public void HandleTurnManagerEvent(TurnStateMachineManagerEventType eventType)
+    {
+        currentState.HandleTurnStateEvent(this, eventType);
+    }
+}
